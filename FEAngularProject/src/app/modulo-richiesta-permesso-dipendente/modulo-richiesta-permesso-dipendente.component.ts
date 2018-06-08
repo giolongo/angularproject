667828abd07749cb4d0f391b9da744624bbabb7a
Orazio Contarino <ocontarino@gmail.com>
2018-06-08 11:25:53 +0200

    modulo richiesta permesso

--- FEAngularProject/src/app/modulo-richiesta-permesso-dipendente/modulo-richiesta-permesso-dipendente.component.ts
+++ FEAngularProject/src/app/modulo-richiesta-permesso-dipendente/modulo-richiesta-permesso-dipendente.component.ts
@@ -6,13 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modulo-richiesta-permesso-dipendente.component.css']
 })
 export class ModuloRichiestaPermessoDipendenteComponent implements OnInit {
-  dataInizio : String;
-  dataFine : String;
-  certificato : String;
-  
-  constructor() { }
+  dataInizio : any;
+  dataFine : any;
+  fileBase64 : String;
+  nomeCertificatoCaricato : String;
+  constructor() { 
+    this.dataInizio = {
+      "giorno" : "",
+      "mese" : "",
+      "anno" : ""
+    };
+    this.dataFine = {
+      "giorno" : "",
+      "mese" : "",
+      "anno" : ""
+    };
+    this.nomeCertificatoCaricato = "";
+  }
 
   ngOnInit() {
   }
 
+  setFileName(event){
+    var file = event.target.files[0];
+    this.nomeCertificatoCaricato = file.name;
+
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      this.fileBase64 = reader.result;
+      //enable send button
+      console.log(this.fileBase64);
+    }.bind(this);
+    reader.onerror = function (error) {
+      alert('Error: '+ error);
+    };
+  }
 }
