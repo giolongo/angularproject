1968c1117ae508bd9905f36780ff07ec1a31a532
Orazio Contarino <ocontarino@gmail.com>
2018-06-09 11:58:02 +0200

    riodianmento moduli gestione dipendenti

--- FEAngularProject/src/app/app.module.ts
+++ FEAngularProject/src/app/app.module.ts
@@ -1,35 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { EmployerLogService } from '../service/employer-log.service';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
+import { DataTablesModule } from 'angular-datatables';
+import { CalendarModule } from 'angular-calendar';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+//Common
+import { EmployerLogService } from '../service/employer-log.service';
+import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+//Gestione dipendenti
 import { RegistraUtenteComponent } from './registra-utente/registra-utente.component';
 import { RegistraTeamComponent } from './registra-team/registra-team.component';
 import { RegistraPermessiComponent } from './registra-permessi/registra-permessi.component';
 import { VisualizzaSkillComponent } from './visualizza-skill/visualizza-skill.component';
 import { VisualizzaTeamComponent } from './visualizza-team/visualizza-team.component';
-import { GestionePermessiComponent } from './gestione-permessi/gestione-permessi.component';
 import { ProfiloUtenteComponent } from './profilo-utente/profilo-utente.component';
-import { RegistraPermessoComponent } from './registra-permesso/registra-permesso.component';
-import { VisualizzaPermessiRichiestiComponent } from './visualizza-permessi-richiesti/visualizza-permessi-richiesti.component';
-import { GestionePermessiDipendentiComponent } from './gestione-permessi-dipendenti/gestione-permessi-dipendenti.component';
-import { GestionePermessiManagerComponent } from './gestione-permessi-manager/gestione-permessi-manager.component';
-import { DatatableListaPermessiManagerComponent } from './datatable-lista-permessi-manager/datatable-lista-permessi-manager.component';
-import { DatatableListaPermessiDipendentiComponent } from './datatable-lista-permessi-dipendenti/datatable-lista-permessi-dipendenti.component';
-import { DataTablesModule } from 'angular-datatables';
-import { CalendarioPermessiDipendenteComponent } from './calendario-permessi-dipendente/calendario-permessi-dipendente.component';
-import { CalendarModule } from 'angular-calendar';
-import { ModuloRichiestaPermessoDipendenteComponent } from './modulo-richiesta-permesso-dipendente/modulo-richiesta-permesso-dipendente.component';
 import { ProfiloUtenteDatiPersonaliComponent } from './profilo-utente-dati-personali/profilo-utente-dati-personali.component';
 import { ProfiloUtenteSkillsComponent } from './profilo-utente-skills/profilo-utente-skills.component';
+//Gestione permessi
+import { GestionePermessiComponent } from './gestione-permessi/gestione-permessi.component';
+import { CalendarioPermessiDipendenteComponent } from './gestione-permessi/calendario-permessi-dipendente/calendario-permessi-dipendente.component';
+import { GestionePermessiDipendentiComponent } from './gestione-permessi/gestione-permessi-dipendenti/gestione-permessi-dipendenti.component';
+import { GestionePermessiManagerComponent } from './gestione-permessi/gestione-permessi-manager/gestione-permessi-manager.component';
+import { DatatableListaPermessiManagerComponent } from './gestione-permessi/gestione-permessi-manager/datatable-lista-permessi-manager/datatable-lista-permessi-manager.component';
+import { DatatableListaPermessiDipendentiComponent } from './gestione-permessi/gestione-permessi-dipendenti/datatable-lista-permessi-dipendenti/datatable-lista-permessi-dipendenti.component';
+import { ModuloRichiestaPermessoDipendenteComponent } from './modulo-richiesta-permesso-dipendente/modulo-richiesta-permesso-dipendente.component';
+//Gestione permessi - deprecati
+import { RegistraPermessoComponent } from './registra-permesso/registra-permesso.component';
+import { VisualizzaPermessiRichiestiComponent } from './visualizza-permessi-richiesti/visualizza-permessi-richiesti.component';
 @NgModule({
   declarations: [
     AppComponent,
