92b9bf0a817320adee498816d83df5d6aa437df5
Giovanni Emanuele Longo <giovanniemanuelelongo@gmail.com>
2018-06-12 22:32:54 +0200

    Fix parametro undef (Da rivedere)

--- FEAngularProject/src/app/skill-dipendente/skill-dipendente.component.ts
+++ FEAngularProject/src/app/skill-dipendente/skill-dipendente.component.ts
@@ -12,6 +12,10 @@ export class SkillDipendenteComponent implements OnInit {
   private idDipendente : String;
   datiDipendente : any;
   constructor(private activatedRoute: ActivatedRoute,  private router: Router, private restRequestService:RestRequestService, private employerLogService : EmployerLogService) {
+
+  }
+
+  ngOnInit() {
     if(!this.employerLogService.isLogged()){
       //Se non lo è lo riporto alla pagina di Login
       if(!sessionStorage.getItem("token")){
@@ -24,21 +28,22 @@ export class SkillDipendenteComponent implements OnInit {
         }.bind(this));
       }
     }
-  }
 
-  ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       if(params.hasOwnProperty('id_dipendente'))
       {
         this.idDipendente = params.id_dipendente;
-        if(this.employerLogService.getId() == this.idDipendente){
+        if(this.idDipendente == "undefined"){
+          this.router.navigate(['/ricercaRisultati']);
+        }
+        else if(this.employerLogService.getId() == this.idDipendente){
           this.router.navigate(['/visualizzaProfilo']);
         }
         this.restRequestService.getDipendeteInfoAndSkill(this.idDipendente).subscribe(function(response){
           this.datiDipendente = response['data'];
           console.log( this.datiDipendente);
         }.bind(this))
-      };
+      }
     });
   }
 
