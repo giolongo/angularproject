85012c7fb9ea944017e007e027b1312a5c5f5791
Giovanni Emanuele Longo <giovanniemanuelelongo@gmail.com>
2020-02-06 23:25:31 +0100

    Ultimata navbar

--- FEAngularProject/src/app/app.component.ts
+++ FEAngularProject/src/app/app.component.ts
@@ -1,27 +1,51 @@
-import { Component } from '@angular/core';
-import { EmployerLogService } from '../service/employer-log.service';
-import { NgModel } from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {Component} from '@angular/core';
+import {EmployerLogService} from '../service/employer-log.service';
 import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
-import { Router } from "@angular/router";
-import * as $ from 'jquery';
+import {Router} from "@angular/router";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [NgbDropdownConfig] // add NgbDropdownConfig to the component providers
+  providers: [NgbDropdownConfig],
+  animations: [
+    trigger('openClosed', [
+      state('open', style({
+        opacity: 1,
+        height: '*'
+      })),
+      state('closed', style({
+        opacity: .2,
+        height: '*'
+      })),
+      transition('open <=> closed', [
+        animate('.2s')
+      ]),
+    ]),
+  ]// add NgbDropdownConfig to the component providers
 })
+
 export class AppComponent {
   searchField;
+  isMenuOpen: boolean = false;
   title = 'Employer Manager';
-  constructor(private employerLogService : EmployerLogService, config: NgbDropdownConfig, private router: Router) {
+
+  constructor(private employerLogService: EmployerLogService, config: NgbDropdownConfig, private router: Router) {
     // customize default values of dropdowns used by this component tree
     config.placement = 'bottom-left';
     config.autoClose = true;
   }
-  
+
   search() {
-    if(!this.searchField) this.searchField="Team, Dipendente";
+    if (!this.searchField) this.searchField = "Team, Dipendente";
     this.router.navigate(['/ricercaRisultati']);
   }
 
@@ -30,17 +54,18 @@ export class AppComponent {
   }
 
 
-  logout(){
+  logout() {
     this.employerLogService.logOut();
   }
+
   ngOnInit() {
-    if(!this.employerLogService.isLogged()){
+    if (!this.employerLogService.isLogged()) {
       //Se non lo è lo riporto alla pagina di Login
-      if(!sessionStorage.getItem("token")){
+      if (!sessionStorage.getItem("token")) {
         this.router.navigate(['/login']);
-      }else{
-        this.employerLogService.refreshSessionByTokenRequest().subscribe(function(response){
-          if(!this.employerLogService.caricaUtenteLoggato(response)){
+      } else {
+        this.employerLogService.refreshSessionByTokenRequest().subscribe(function (response) {
+          if (!this.employerLogService.caricaUtenteLoggato(response)) {
             this.router.navigate(['/login']);
           }
         }.bind(this));
--- FEAngularProject/src/app/app.module.ts
+++ FEAngularProject/src/app/app.module.ts
@@ -1,43 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations'
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { DataTablesModule } from 'angular-datatables';
-import { CalendarModule } from 'angular-calendar';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations'
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {DataTablesModule} from 'angular-datatables';
+import {CalendarModule} from 'angular-calendar';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 //Common
-import { EmployerLogService } from '../service/employer-log.service';
-import { LoginComponent } from './login/login.component';
-import { RegistraUtenteComponent } from './registra-utente/registra-utente.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {EmployerLogService} from '../service/employer-log.service';
+import {LoginComponent} from './login/login.component';
+import {RegistraUtenteComponent} from './registra-utente/registra-utente.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 //Gestione dipendenti
-import { RegistraTeamComponent } from './registra-team/registra-team.component';
-import { ProfiloUtenteComponent } from './profilo-utente/profilo-utente.component';
-import { ProfiloUtenteDatiPersonaliComponent } from './profilo-utente-dati-personali/profilo-utente-dati-personali.component';
-import { ProfiloUtenteSkillsComponent } from './profilo-utente-skills/profilo-utente-skills.component';
+import {RegistraTeamComponent} from './registra-team/registra-team.component';
+import {ProfiloUtenteComponent} from './profilo-utente/profilo-utente.component';
+import {ProfiloUtenteDatiPersonaliComponent} from './profilo-utente-dati-personali/profilo-utente-dati-personali.component';
+import {ProfiloUtenteSkillsComponent} from './profilo-utente-skills/profilo-utente-skills.component';
 //Gestione permessi
-import { GestionePermessiComponent } from './gestione-permessi/gestione-permessi.component';
-import { CalendarioPermessiDipendenteComponent } from './gestione-permessi/calendario-permessi-dipendente/calendario-permessi-dipendente.component';
-import { GestionePermessiDipendentiComponent } from './gestione-permessi/gestione-permessi-dipendenti/gestione-permessi-dipendenti.component';
-import { GestionePermessiManagerComponent } from './gestione-permessi/gestione-permessi-manager/gestione-permessi-manager.component';
-import { DatatableListaPermessiManagerComponent } from './gestione-permessi/gestione-permessi-manager/datatable-lista-permessi-manager/datatable-lista-permessi-manager.component';
-import { DatatableListaPermessiDipendentiComponent } from './gestione-permessi/gestione-permessi-dipendenti/datatable-lista-permessi-dipendenti/datatable-lista-permessi-dipendenti.component';
-import { ModuloRichiestaPermessoDipendenteComponent } from './gestione-permessi/registra-permessi/modulo-richiesta-permesso-dipendente/modulo-richiesta-permesso-dipendente.component';
+import {GestionePermessiComponent} from './gestione-permessi/gestione-permessi.component';
+import {CalendarioPermessiDipendenteComponent} from './gestione-permessi/calendario-permessi-dipendente/calendario-permessi-dipendente.component';
+import {GestionePermessiDipendentiComponent} from './gestione-permessi/gestione-permessi-dipendenti/gestione-permessi-dipendenti.component';
+import {GestionePermessiManagerComponent} from './gestione-permessi/gestione-permessi-manager/gestione-permessi-manager.component';
+import {DatatableListaPermessiManagerComponent} from './gestione-permessi/gestione-permessi-manager/datatable-lista-permessi-manager/datatable-lista-permessi-manager.component';
+import {DatatableListaPermessiDipendentiComponent} from './gestione-permessi/gestione-permessi-dipendenti/datatable-lista-permessi-dipendenti/datatable-lista-permessi-dipendenti.component';
+import {ModuloRichiestaPermessoDipendenteComponent} from './gestione-permessi/registra-permessi/modulo-richiesta-permesso-dipendente/modulo-richiesta-permesso-dipendente.component';
 //Gestione permessi - deprecati
-import { RegistraPermessiComponent } from './gestione-permessi/registra-permessi/registra-permessi.component';
-import { RicercaComponent } from './ricerca/ricerca.component';
-import { DatatableListaDipendentiComponent } from './ricerca/datatable-lista-dipendenti/datatable-lista-dipendenti.component';
-import { DatatableListaTeamComponent } from './ricerca/datatable-lista-team/datatable-lista-team.component';
-import { SkillDipendenteComponent } from './skill-dipendente/skill-dipendente.component';
-import { SkillDipendenteDipendentiComponent } from './skill-dipendente/skill-dipendente-dipendenti/skill-dipendente-dipendenti.component';
-import { SkillDipendenteManagerComponent } from './skill-dipendente/skill-dipendente-manager/skill-dipendente-manager.component';
-import { TeamComponent } from './team/team.component';
-import { TeamManagerComponent } from './team/team-manager/team-manager.component';
-import { LaddaModule } from 'angular2-ladda';
-import { UtilsService } from '../service/utils.service';
+import {RegistraPermessiComponent} from './gestione-permessi/registra-permessi/registra-permessi.component';
+import {RicercaComponent} from './ricerca/ricerca.component';
+import {DatatableListaDipendentiComponent} from './ricerca/datatable-lista-dipendenti/datatable-lista-dipendenti.component';
+import {DatatableListaTeamComponent} from './ricerca/datatable-lista-team/datatable-lista-team.component';
+import {SkillDipendenteComponent} from './skill-dipendente/skill-dipendente.component';
+import {SkillDipendenteDipendentiComponent} from './skill-dipendente/skill-dipendente-dipendenti/skill-dipendente-dipendenti.component';
+import {SkillDipendenteManagerComponent} from './skill-dipendente/skill-dipendente-manager/skill-dipendente-manager.component';
+import {TeamComponent} from './team/team.component';
+import {TeamManagerComponent} from './team/team-manager/team-manager.component';
+import {LaddaModule} from 'angular2-ladda';
+import {UtilsService} from '../service/utils.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -65,7 +66,7 @@ import { UtilsService } from '../service/utils.service';
     TeamComponent,
     TeamManagerComponent,
   ],
-  imports: [    
+  imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
@@ -74,8 +75,10 @@ import { UtilsService } from '../service/utils.service';
     NgbModule.forRoot(),
     CalendarModule.forRoot(),
     LaddaModule,
+    BrowserAnimationsModule
   ],
-  providers: [EmployerLogService,UtilsService],
+  providers: [EmployerLogService, UtilsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
